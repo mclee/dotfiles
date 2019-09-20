d0bb49020ca47b000f656f796522fc3b879558f7
George Lee <mclee@foolme.net>
2019-09-20 08:59:16 +0800

    add neovim

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ if [ $MACHINE != "Darwin" ]; then
 	# setup default packages first
 	sudo apt-get update
 	sudo apt-get -y install libssl-dev build-essential exuberant-ctags libreadline-dev imagemagick \
-		libmysqlclient-dev byobu libpcre++-dev libxml2-dev libxslt1-dev htop
+		libmysqlclient-dev byobu libpcre++-dev libxml2-dev libxslt1-dev htop neovim
 
 	# install tsar
 	git clone https://github.com/alibaba/tsar.git ~/tsar
